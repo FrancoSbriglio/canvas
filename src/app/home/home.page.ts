ab5cfc10e961270dd461250aa699f7cba3422c23
unknown <franco.sbriglio@gmail.com>
2019-08-07 22:09:11 -0300

    version 1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, Renderer } from '@angular/core';
+import { NavController, Platform } from '@ionic/angular'; // aca normalize
+import { File, IWriteOptions } from '@ionic-native/file';
+import { Storage } from '@ionic/storage';
+import { IonContent } from '@ionic/angular';
+import { inject } from '@angular/core/testing';
 
-@Component({
+
+
+const STORAGE_KEY = 'IMAGE_LIST';
+
+
+@Component({ // aca
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+ // styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage  {
+  @ViewChild('imageCanvas', {static: false}) canvas: any;
 
-  constructor() {}
+  canvasElement: any;
+
+  saveX: number;
+  saveY: number;
+
+  storedImages = [];
+
+// tslint:disable-next-line: max-line-length
+constructor( public navCtrl: NavController, private file: File, private storage: Storage, public renderer: Renderer, private plt: Platform) {
+  // Load all stored images when the app is ready
+  this.storage.ready().then(() => {
+    this.storage.get(STORAGE_KEY).then(data => {
+      if (data !== undefined) {
+        this.storedImages = data;
+      }
+    });
+  });
+}
+
+
+
+  // Make Canvas sticky at the top stuff
+  @ViewChild(IonContent, {static: false}) content: IonContent;
+
+  @ViewChild('fixedContainer', {static: false}) fixedContainer: any;
+
+
+  // Color Stuff
+  selectedColor = '#9e2956';
+
+  colors = [ '#9e2956', '#c2281d', '#de722f', '#edbf4c', '#5db37e', '#459cde', '#4250ad', '#802fa3' ];
+
+
+  ionViewDidEnter() {
+    // https://github.com/ionic-team/ionic/issues/9071#issuecomment-362920591
+    // Get the height of the fixed item
+    let itemHeight = this.fixedContainer.nativeElement.offsetHeight;
+    const scroll = this.content.getScrollElement();
+
+    // Add preexisting scroll margin to fixed container size
+    itemHeight = Number.parseFloat(scroll.style.marginTop.replace('px', '')) + itemHeight;
+    scroll.style.marginTop = itemHeight + 'px';
+  }
+
+  ionViewDidLoad() {
+    // Set the Canvas Element and its size
+    this.canvasElement = this.canvas.nativeElement;
+    this.canvasElement.width = this.plt.width() + '';
+    this.canvasElement.height = 200;
+  }
 
 }
+
