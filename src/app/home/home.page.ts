8c7a8aa737702c4f4cee54a4ccf4c0c8d8cebe82
unknown <franco.sbriglio@gmail.com>
2019-08-07 22:52:40 -0300

    version 2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, ViewChild, Renderer } from '@angular/core';
 import { NavController, Platform } from '@ionic/angular'; // aca normalize
-import { File, IWriteOptions } from '@ionic-native/file';
+// import { File, IWriteOptions } from '@ionic-native/file';
 import { Storage } from '@ionic/storage';
 import { IonContent } from '@ionic/angular';
-import { inject } from '@angular/core/testing';
+
 
 
 
@@ -13,8 +13,9 @@ const STORAGE_KEY = 'IMAGE_LIST';
 @Component({ // aca
   selector: 'app-home',
   templateUrl: 'home.page.html',
- // styleUrls: ['home.page.scss']
+  styleUrls: ['home.page.scss'],
 })
+
 export class HomePage  {
   @ViewChild('imageCanvas', {static: false}) canvas: any;
 
@@ -26,7 +27,7 @@ export class HomePage  {
   storedImages = [];
 
 // tslint:disable-next-line: max-line-length
-constructor( public navCtrl: NavController, private file: File, private storage: Storage, public renderer: Renderer, private plt: Platform) {
+constructor( public navCtrl: NavController,  private storage: Storage, public renderer: Renderer, private plt: Platform) {
   // Load all stored images when the app is ready
   this.storage.ready().then(() => {
     this.storage.get(STORAGE_KEY).then(data => {
@@ -68,6 +69,81 @@ constructor( public navCtrl: NavController, private file: File, private storage:
     this.canvasElement.width = this.plt.width() + '';
     this.canvasElement.height = 200;
   }
-
+  selectColor(color) {
+    this.selectedColor = color;
+  }
+   
+  startDrawing(ev) {
+    var canvasPosition = this.canvasElement.getBoundingClientRect();
+   
+    this.saveX = ev.touches[0].pageX - canvasPosition.x;
+    this.saveY = ev.touches[0].pageY - canvasPosition.y;
+  }
+   
+  moved(ev) {
+    var canvasPosition = this.canvasElement.getBoundingClientRect();
+   
+    let ctx = this.canvasElement.getContext('2d');
+    let currentX = ev.touches[0].pageX - canvasPosition.x;
+    let currentY = ev.touches[0].pageY - canvasPosition.y;
+   
+    ctx.lineJoin = 'round';
+    ctx.strokeStyle = this.selectedColor;
+    ctx.lineWidth = 5;
+   
+    ctx.beginPath();
+    ctx.moveTo(this.saveX, this.saveY);
+    ctx.lineTo(currentX, currentY);
+    ctx.closePath();
+   
+    ctx.stroke();
+   
+    this.saveX = currentX;
+    this.saveY = currentY;
+  }
+   
+   
+  saveCanvasImage() {
+    var dataUrl = this.canvasElement.toDataURL();
+   
+    let ctx = this.canvasElement.getContext('2d');
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // Clears the canvas
+   
+    let name = new Date().getTime() + '.png';
+    let path = this.file.dataDirectory;
+    let options: IWriteOptions = { replace: true };
+   
+    var data = dataUrl.split(',')[1];
+    let blob = this.b64toBlob(data, 'image/png');
+   
+    this.file.writeFile(path, name, blob, options).then(res => {
+      this.storeImage(name);
+    }, err => {
+      console.log('error: ', err);
+    });
+  }
+  // https://forum.ionicframework.com/t/save-base64-encoded-image-to-specific-filepath/96180/3
+  b64toBlob(b64Data, contentType) {
+    contentType = contentType || '';
+    var sliceSize = 512;
+    var byteCharacters = atob(b64Data);
+    var byteArrays = [];
+   
+    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      var slice = byteCharacters.slice(offset, offset + sliceSize);
+   
+      var byteNumbers = new Array(slice.length);
+      for (var i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+   
+      var byteArray = new Uint8Array(byteNumbers);
+   
+      byteArrays.push(byteArray);
+    }
+   
+    var blob = new Blob(byteArrays, { type: contentType });
+    return blob;
+  }
 }
 
